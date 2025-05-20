5057e2ef68e472cf94e6500504917354f987d90c
vojta_mi <tami.vojkel@gmail.com>
2025-05-20 14:56:58 +0200

    age verification between sessions

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,41 +1,5 @@
-// Function to highlight the active page link
 document.addEventListener("DOMContentLoaded", function () {
-    var navLinks = document.querySelectorAll('.nav-link');
-    var currentUrl = new URL(window.location.href).pathname;
-
-    navLinks.forEach(function (link) {
-        if (link.getAttribute('href') && link.getAttribute('href') !== '#') {
-            var linkUrl = new URL(link.href).pathname;
-            if (linkUrl === currentUrl) {
-                link.classList.add('active');
-                link.setAttribute('aria-current', 'page');
-            }
-        }
-    });
-});
-
-
-
-// Function to highlight the active language link
-document.addEventListener("DOMContentLoaded", function () {
-    var langLinks = document.querySelectorAll('.lang-link');
-
-    // Extract the current language from the URL path (e.g., /cs/ or /en/)
-    var currentLang = window.location.pathname.split('/')[1]; // Get the first part of the path
-
-    langLinks.forEach(function (link) {
-        var linkLang = link.getAttribute('href').split('/')[1]; // Get the first part of the href path
-        if (linkLang === currentLang) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const ageConfirmed = sessionStorage.getItem("ageConfirmed");
+    const ageConfirmed = localStorage.getItem("ageConfirmed");
     if (ageConfirmed) return;
 
     function showAgeModal() {
@@ -48,9 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
         document.removeEventListener("scroll", showAgeModal);
     }
 
-    // Confirm age and store in sessionStorage
+    // Confirm age and store in localStorage
     document.getElementById("confirm-age").addEventListener("click", function () {
-        sessionStorage.setItem("ageConfirmed", true);
+        localStorage.setItem("ageConfirmed", true);
         const ageModal = bootstrap.Modal.getInstance(document.getElementById('ageVerificationModal'));
         ageModal.hide();
     });
@@ -64,5 +28,3 @@ document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("click", showAgeModal);
     document.addEventListener("scroll", showAgeModal);
 });
-
-
